cb1425e70700d8062a1162a46b1069fec7247bcb
karen <karen@mail.com>
2022-07-28 15:08:46 +0300

    hello

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Car from './Car';
 
+// sfc
 function App() {
   return (
     <div className="App">
--- src/Car.jsx
+++ src/Car.jsx
@@ -1,4 +1,5 @@
 import  { Component } from 'react';
+// imrc
 
 class Car extends Component {
     render() {  
