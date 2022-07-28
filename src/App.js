471244ddd4c232e34c11840d678b417abd0662f7
karen <karen@mail.com>
2022-07-28 09:23:52 +0300

    props in classComponent

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Car moudel="BMW" color="RED" />
       <Car moudel="BOGATI" color="ORANGE" />
       <Car moudel="MAZARATI" color="BLUE" />
-    </div>
+    </div> 
   );
 }
 
--- src/Car.jsx
+++ src/Car.jsx
@@ -2,9 +2,9 @@ import  { Component } from 'react';
 
 class Car extends Component {
     render() { 
-        return (
+        return ( 
             <p>the model of car is {this.props.moudel} and the color is {this.props.color}</p>
-        );
+        ); 
     }
 }
  
